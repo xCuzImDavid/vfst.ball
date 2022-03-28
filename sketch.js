a92808c1f304cd527024cf58ff755038a25bdf4a
David Althaus <david24.althaus@gmail.com>
2022-03-28 13:20:04 +0200

    Ball Kollisionen

--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+
 class ball {
     constructor(pos,radius,momentum){
         this.pos = pos;
@@ -16,34 +17,58 @@ class ball {
     collidesy(canvasheight) {
         return (this.pos.y < this.radius) || (this.pos.y > canvasheight - this.radius);
     }
+    collideswith( ball ){
+        return dist( this.pos.x, this.pos.y, ball.pos.x, ball.pos.y) <= this.radius + ball.radius;
+    }
 }
 
 function setup() {
-    createCanvas(400, 400);
+    cheight = 600;
+    cwidth = 800;
+    createCanvas(cwidth, cheight);
     frameRate(40);
-    ball1 = new ball(createVector(200, 100), 10, createVector(10, -10));
-    ball2 = new ball(createVector(100, 200), 15, createVector(5, -5));
+    balls = [];
+    for( x=0; x<= 10; x++)
+    {
+        balls.push( new ball( createVector(10*x+40, 10*x+40), 10, createVector( x+1, -x+1)));
+    }
 }
 
 function draw() {
     background(220);
-    //x = x + a
-    //y = y + a
-    ball1.addmomentum();
-    if (ball1.collidesx(400)) {
-        ball1.momentum.x = -1 * ball1.momentum.x;
-    }
-    if (ball1.collidesy(400)) {
-        ball1.momentum.y = ball1.momentum.y * -1;
-    }
-    ball1.draw();
+    for( x=0; x < balls.length; x++ )
+    {
+        b = balls[x];
+        b.addmomentum();
+        if (b.collidesx(cwidth)) {
+            b.momentum.x = -1 * b.momentum.x;
+        }
+        if (b.collidesy(cheight)) {
+            b.momentum.y = b.momentum.y * -1;
+        }
 
-    ball2.addmomentum();
-    if (ball2.collidesx(400)) {
-        ball2.momentum.x = -1 * ball2.momentum.x;
     }
-    if (ball2.collidesy(400)) {
-        ball2.momentum.y = ball2.momentum.y * -1;
+
+    for ( x=0; x < balls.length; x++ )
+    {
+        b = balls [x];
+        for ( p = 0; p < balls.length; p++ )
+        {
+            if (x === p) {
+                continue;
+            }
+            bother = balls[p];
+            b.collideswith(bother);
+            if (b.collideswith(bother)) {
+                b.momentum.x = -1 * b.momentum.x;
+                b.momentum.y = -1 * b.momentum.y;
+            }
+        }
     }
-    ball2.draw();
+
+    for( x=0; x < balls.length; x++ )
+    {
+        b = balls[x];
+        b.draw();
+    }
 }
